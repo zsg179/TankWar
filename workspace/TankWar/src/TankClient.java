caceca23be2bbd573da14328f5f94798a027c08c
zsg179 <18977330642@163.com>
2020-04-02 12:41:27 +0800

    0.4 坦克移动

--- workspace/TankWar/src/TankClient.java
+++ workspace/TankWar/src/TankClient.java
@@ -1,6 +1,5 @@
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 /**
  * @Auther :朱树广
@@ -10,21 +9,49 @@ import java.awt.event.WindowEvent;
  */
 public class TankClient extends Frame {
 
+    int x = 50, y = 50;
+
+    @Override
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, 30, 30);
+        g.setColor(c);
+        y += 5;
+    }
+
     public void launchFrame() {
         this.setLocation(400, 300);
-        this.setSize(800,600);
+        this.setSize(800, 600);
+        this.setTitle("TankWar");
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-        this.setTitle("TankWar");
         this.setResizable(false);
+        this.setBackground(Color.GREEN);
         this.setVisible(true);
+        new Thread(new PaintThread()).start();
     }
+
     public static void main(String[] args) {
         TankClient tc = new TankClient();
         tc.launchFrame();
+    }
+
+    private class PaintThread implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                repaint();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
