23cfe87acbfc16b74f96962e84cbb7f487c832a8
zsg179 <18977330642@163.com>
2020-04-02 12:03:36 +0800

    关闭窗口，禁止调整窗口大小

--- workspace/TankWar/src/TankClient.java
+++ workspace/TankWar/src/TankClient.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * @Auther :朱树广
@@ -11,6 +13,14 @@ public class TankClient extends Frame {
     public void launchFrame() {
         this.setLocation(400, 300);
         this.setSize(800,600);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setTitle("TankWar");
+        this.setResizable(false);
         this.setVisible(true);
     }
     public static void main(String[] args) {
